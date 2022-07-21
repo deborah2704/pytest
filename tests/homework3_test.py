9312b28c0a7ed406dfacbcf17f94a623ed96fb6e
deborah2704 <deborah270401@gmail.com>
2022-07-21 13:13:00 +0300

    Create unit tests for each function of the homework 3

--- tests/homework3_test.py
+++ tests/homework3_test.py
@@ -1,14 +1,25 @@
 from homework3 import split_male_female, find_median_average, print_values_above
 import pytest
 
-def test_split_male_female(capfd):
-    data_set = {
+data_set = {
             0: {"name": "deborah", "age": 21, "sex": "female"},
             1: {"name": "esther", "age": 25, "sex": "female"},
             2: {"name": "noa", "age": 34, "sex": "male"}
             }
+
+def test_split_male_female(capfd):
     split_male_female(data_set)
     out, err = capfd.readouterr()
     assert out == ("[{'name': 'noa', 'age': 34, 'sex': 'male'}]\n"
  "[{'name': 'deborah', 'age': 21, 'sex': 'female'}, {'name': 'esther', 'age': "
  "25, 'sex': 'female'}]\n")
+
+def test_find_median_average(capfd):
+    find_median_average(data_set)
+    out, err = capfd.readouterr()
+    assert out == ("[21, 25, 34]\n26.666666666666668\n")
+
+def test_print_values_above(capfd):
+    print_values_above(data_set, 22)
+    out, err = capfd.readouterr()
+    assert out == ("{1: {'name': 'esther', 'age': 25, 'sex': 'female'}, 2: {'name': 'noa', 'age': 34, 'sex': 'male'}}\n")
